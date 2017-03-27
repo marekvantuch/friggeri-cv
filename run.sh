9424a5aee5baba84f6849bba409aabdc43fc441f
Marek Vantuch <marek.vantuch@gmail.com>
2017-03-27 17:07:55 +1300

    Fix the dates formatting

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-latexmk -xelatex -synctex=1 mvantuch.tex
+FILE="mvantuch-nz.tex"
+#FILE="friggeri-cv-default.tex"
+
+latexmk -f -halt-on-error -xelatex -synctex=1 -auxdir=./tmp ${FILE}
